cd16d0483341cb5ee166985bc9640a106fc5f74e
RandomPGR301 <random@mail.com>
2018-12-02 15:29:48 +0100

    changed app-prefix and pipeline default name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 # Modify this file to make it your own :-)
 
 variable "app_prefix" {
-  default = "pgr301-exam-app"
+  default = "pgr301-exam"
 }
 
 variable "pipeline_name" {
-  default = "pgr301-exam-app"
+  default = "pgr301-exam"
 }
